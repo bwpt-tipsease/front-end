222a1b5ab9154454c028abbafe83b1ec343a9ac8
Craig Bourne <craigbourne15@gmail.com>
2019-09-19 23:06:08 +0100

    added yup validation schema for password and email inputs

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withFormik, Field, Form } from 'formik';
+import * as yup from 'yup';
 
 const LoginForm = props => {
 	return (
@@ -18,6 +19,10 @@ export default withFormik({
 			password: props.password
 		};
 	},
+	validationSchema: yup.object().shape({
+		email: yup.string().email().required('* Email is required'),
+		password: yup.string().min(8, '* Password must be 8 characters').required('* Password is required')
+	}),
 	handleSubmit: values => {
 		console.log(values);
 	}
