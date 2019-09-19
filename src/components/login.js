b9b1ae1bc34825a35b58cb6ddfda4a86998b9c05
Craig Bourne <craigbourne15@gmail.com>
2019-09-19 17:25:21 +0100

    basic login form component

---
+++ src/components/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginForm = props => {
+	return (
+		<form>
+			<input type="text" name="" id="" />
+			<button type="submit" />
+		</form>
+	);
+};
+
+export default LoginForm;
