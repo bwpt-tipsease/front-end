6165485dd51f2f8cb0472c8727d2962092be2aef
Craig Bourne <craigbourne15@gmail.com>
2019-09-20 19:45:11 +0100

    created basic worker card component, with hardcoded text

---
+++ src/components/worker/WorkerCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function WorkerCard(props) {
+	return (
+		<div>
+			<h1>This is the h1</h1>
+			<h2>This is an h2</h2>
+			<h2>This is an h2</h2>
+		</div>
+	);
+}
+
+export default WorkerCard;
