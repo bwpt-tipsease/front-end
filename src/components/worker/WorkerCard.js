9d129bf97953fa63c3b51744362e25fafd18d208
Craig Bourne <craigbourne15@gmail.com>
2019-09-21 03:24:43 +0100

    made each worker card a link. Just linking to google for now

--- src/components/worker/WorkerCard.js
+++ src/components/worker/WorkerCard.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 function WorkerCard(props) {
 	return (
-		<div className="worker-card">
-			<img src={props.worker.avatar} />
-			<h1>
-				{props.worker.first_name} {props.worker.last_name}
-			</h1>
-			<h4>Tagline: {props.worker.email}</h4>
-			<h4>Time in job: {props.worker.id} months</h4>
-		</div>
+		<a href="http://www.google.com">
+			<div className="worker-card">
+				<img src={props.worker.avatar} />
+				<h1>
+					{props.worker.first_name} {props.worker.last_name}
+				</h1>
+				<h4>Tagline: {props.worker.email}</h4>
+				<h4>Time in job: {props.worker.id} months</h4>
+			</div>
+		</a>
 	);
 }
 
