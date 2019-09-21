32c0812d9427b32a3b43622c409d6244dea16888
Craig Bourne <craigbourne15@gmail.com>
2019-09-21 02:06:05 +0100

    import worker card & display on page

--- src/components/worker/WorkerList.js
+++ src/components/worker/WorkerList.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import WorkerCard from './WorkerCard';
 
 function WorkerList(props) {
-	return <div className="worker-list">hello</div>;
+	return (
+		<div className="worker-list">
+			<WorkerCard />
+		</div>
+	);
 }
 
 export default WorkerList;
