b0a89599df8a179f09ac8e99741918b6ac23ddaa
Craig Bourne <craigbourne15@gmail.com>
2019-09-21 02:08:36 +0100

    add axios, useState & useEffect + set the state & get some dummy data in

--- src/components/worker/WorkerList.js
+++ src/components/worker/WorkerList.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import WorkerCard from './WorkerCard';
 
 function WorkerList(props) {
+	const [ data, setData ] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://reqres.in/api/users')
+			.then(res => {
+				setData(res.data.data);
+				console.log('data', res.data.data);
+			})
+			.catch(err => console.log(err));
+	}, []);
+
 	return (
 		<div className="worker-list">
 			<WorkerCard />
