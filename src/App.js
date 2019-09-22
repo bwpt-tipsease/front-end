f46e23290bb4f668da753588df5d629ec3335956
Craig Bourne <craigbourne15@gmail.com>
2019-09-23 00:56:13 +0100

    set up routes to the login form and the list of workers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import WorkerList from './components/worker/WorkerList';
+import { Route } from 'react-router-dom';
 import LoginForm from './components/forms/Login';
+import WorkerList from './components/worker/WorkerList';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			{/* These will need to be separated into routes */}
-			<WorkerList />
-			<LoginForm />
+			<Route exact path="/" component={LoginForm} />
+			<Route path="/workers" component={WorkerList} />
 		</div>
 	);
 }
