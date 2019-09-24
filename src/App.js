5bb10d2d59ea2eaa19934cabff6b061b5c81ed17
Craig Bourne <craigbourne15@gmail.com>
2019-09-24 02:07:54 +0100

    routing setup for workers & worker cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Link } from 'react-router-dom';
 import LoginForm from './components/forms/Login';
-import WorkerList from './components/worker/WorkerList';
+import ServiceWorkerList from './components/serviceWorkers/ServiceWorkerList';
+import ServiceWorkerCard from './components/serviceWorkers/ServiceWorkerCard';
+
 import './App.css';
+import workerData from './data';
 
 function App() {
+	const [ workers, setWorker ] = useState(workerData);
+	console.log(workers);
 	return (
 		<div className="App">
+			{/* Routes */}
 			<Route exact path="/" component={LoginForm} />
-			<Route path="/workers" component={WorkerList} />
+			<Route exact path="/workers" render={props => <ServiceWorkerList workers={workers} {...props} />} />
+			<Route path="/workers/:id" render={props => <ServiceWorkerCard workers={workers} {...props} />} />
 		</div>
 	);
 }
