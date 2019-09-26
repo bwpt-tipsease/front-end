d67e482384d8cc103abadbbe19c8b8c8978455a8
Craig Bourne <craigbourne15@gmail.com>
2019-09-26 01:39:35 +0100

    simplifying signup form

--- src/components/forms/SignUp.js
+++ src/components/forms/SignUp.js
@@ -4,14 +4,12 @@ import { withFormik, Field, Form } from 'formik';
 import * as yup from 'yup';
 
 const SignUpForm = ({ errors, touched, status, resetForm }) => {
-	// console.log('status', status);
 	const [ users, setUsers ] = useState([]);
 
 	useEffect(
 		() => {
 			if (status) {
-				// sets users stored in state to the status - which is response.data
-				setUsers((prevUsers) => [ ...prevUsers, status ]);
+				setUsers(prevUsers => [ ...prevUsers, status ]);
 			}
 		},
 		[ status ]
@@ -19,10 +17,9 @@ const SignUpForm = ({ errors, touched, status, resetForm }) => {
 
 	return (
 		<div>
-			<h1>Sign Up</h1>
 			<Form>
 				{touched.username && errors.username && <p className="error">{errors.username}</p>}
-				<Field type="text" name="username" placeholder="User Name" />
+				<Field type="text" name="username" placeholder="Username" />
 
 				{touched.email && errors.email && <p className="error">{errors.email}</p>}
 				<Field type="email" name="email" placeholder="Email" />
@@ -30,51 +27,20 @@ const SignUpForm = ({ errors, touched, status, resetForm }) => {
 				{touched.password && errors.password && <p className="error">{errors.password}</p>}
 				<Field type="password" name="password" placeholder="Password" autoComplete="" />
 
-				{touched.confirmpassword && errors.confirmpassword && <p className="error">{errors.confirmpassword}</p>}
-				<Field type="password" name="confirmpassword" placeholder="Confirm Password" autoComplete="" />
-
-
-				{touched.creditcardnumber && errors.creditcardnumber && <p className="error">{errors.creditcardnumber}</p>}
-				<Field type="number" name="creditcardnumber" placeholder="Credti Card Number" autoComplete="" />
-
- 				{touched.expdate && errors.expdate && <p className="error">{errors.expdate}</p>}
-				<Field type="number" name="expdate" placeholder="Expiration Date" autoComplete="" />
-				
-				{touched.cvcnumbers && errors.cvcnumbers && <p className="error">{errors.cvcnumbers}</p>}
-				<Field type="numbers" name="cvcnumbers" placeholder="CVC Numbers" autoComplete="" />
-
-			<h2> Billing Address </h2>
-
-				{touched.firstname && errors.firstname && <p className="error">{errors.firstname}</p>}
-				<Field type="text" name="firstname" placeholder="First Name" autoComplete="" />
-
-				{touched.lastname && errors.lastname && <p className="error">{errors.lastname}</p>}
-				<Field type="text" name="lastname" placeholder="Last Name" autoComplete="" />
-
-				{touched.streetaddress && errors.streetaddress && <p className="error">{errors.streetaddress}</p>}
-				<Field type="text" name="streetaddress" placeholder="Street Address" autoComplete="" />
-
-				{touched.zipcode && errors.zipcode && <p className="error">{errors.zipcode}</p>}
-				<Field type="number" name="zipcode" placeholder="Zip Code" autoComplete="" />
-
-
 				<button type="submit">Submit</button>
 			</Form>
 
-			{users.map(
-				(user, index) => (
-					<div key={index} className="user-info">
-						<p>Email: {user.email}</p>
-					</div>
-				)
-				// console.log('user', user)
-			)}
+			{users.map((user, index) => (
+				<div key={index} className="user-info">
+					<p>Email: {user.email}</p>
+				</div>
+			))}
 		</div>
 	);
 };
 
 export default withFormik({
-	mapPropsToValues: (props) => {
+	mapPropsToValues: props => {
 		return {
 			firstname: props.firstname || '',
 			lastname: props.lastname || '',
@@ -92,10 +58,10 @@ export default withFormik({
 
 		axios
 			.post('https://reqres.in/api/users', values)
-			.then((response) => {
+			.then(response => {
 				setStatus(response.data);
 			})
-			.catch((error) => console.log(error));
+			.catch(error => console.log(error));
 
 		resetForm();
 	}
